8382c0044329bd8f3881ee347a1fa3d6f85c12c9
Sid Gilela <sgilela@ucsc.edu>
2017-01-30 04:23:54 -0800

    pushing files into repo

---
+++ p1/Extra/client2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>          
+#include <sys/socket.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define MAXLINE 4096
+
+int     sockfd, n;
+char    recvline[MAXLINE + 1], sendline[1024];
+struct sockaddr_in servaddr;
+int port;
+
+void numArgs(int argc){
+    if (argc != 3){
+        perror("usage: a.out <IPaddress> <PORTnumber>\n");
+        exit(1);
+    }
+}
+
+int createSocket(){
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        perror("socket error\n");
+        exit(1);
+    }
+    return sockfd;
+}
+
+void createServer(const char *ipaddr, const char *portnum){
+    sscanf(portnum,"%d",&port);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);  /* daytime server */
+    if (inet_pton(AF_INET, ipaddr, &servaddr.sin_addr) <= 0){
+        perror("inet_pton error");
+        exit(1);
+    }
+}
+
+void connectSocket(int sockfd){      
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+        perror("connect error\n");
+        exit(1);
+    }
+}
+
+void readSocket2(int sockfd){
+  //sprintf(sendline, "hello world");
+  while(1){
+        bzero(sendline,1024);
+        printf("Enter a command > ");
+        fgets(sendline, 1024, stdin);
+         
+        if( send(sockfd , sendline , strlen(sendline) , 0) < 0)
+        {
+            perror("Send failed\n");
+            exit(1);
+        } 
+            
+        if( recv(sockfd , recvline , 4097 , 0) < 0)
+        {
+            //printf(recvline);
+            //printf("\n");
+            //printf("Got here");
+            perror("recv failed on client\n");
+            exit(1); 
+        }
+        if (strncmp(recvline,"exit",4) == 0){
+            close(sockfd);
+            exit(0);
+        }
+        printf("Server reply : ");
+        printf(recvline);
+        printf("\n");
+        bzero(recvline,1024);
+  }
+}
+
+
+int
+main(int argc, char **argv)
+{
+    numArgs(argc);
+    int sockfd = createSocket();
+    createServer(argv[1],argv[2]);
+    connectSocket(sockfd);
+    readSocket2(sockfd);
+    return 0;
+}
+
+
+
---
+++ p1/Extra/server3.c
@@ -0,0 +1,153 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>          
+#include <sys/socket.h>
+#include <strings.h>
+#include <unistd.h>
+//#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define MAXLINE 4096
+#define LISTENQ 1024
+
+int     listenfd, connfd,read_size;
+struct sockaddr_in servaddr;
+char    buff[1025];
+time_t ticks;
+int port;
+FILE *in;
+extern FILE *popen();
+
+void numArgs(int argc){
+    if (argc != 2){
+        perror("usage: a.out <PORTnumber>\n");
+        exit(1);
+    }
+}
+
+int createListenSocket(){
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0){
+        perror("socket error\n");
+        exit(1);
+    }
+    return listenfd;
+}
+
+void createServer(const char *portnum){
+    sscanf(portnum,"%d",&port);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port); /* daytime server */
+}
+
+void bindServer(int listenfd){
+    int bindID = bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    if (bindID < 0){
+        perror("bind error\n");
+        exit(1);
+    } 
+}
+
+void listenServer(int listenfd){
+    int listenID = listen(listenfd, LISTENQ);
+    if (listenID < 0){
+        perror("listen error\n");
+        exit(1);
+    } 
+}
+
+int acceptServer(int listenfd){
+        connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+        if (connfd < 0){
+            perror("accept error\n");
+            exit(1);
+        } 
+        return connfd;
+}
+
+void readWriteServer(int connfd, int listenfd){
+  while( (read_size = recv(connfd , buff , 1024 , 0)) > 0 )
+    {
+        //Send the message back to client
+        //char 
+
+        if (!(in = popen(buff, "r"))) {
+          perror("Not able to read stream");
+          exit(1);
+        } 
+
+        //int length = (int)strlen(buff);
+        //printf("gets before while loop %d\n",length);
+
+        if(fgets(buff,sizeof(buff), in) == NULL){
+            printf("success in finding problem");
+            sprintf(buff,"empty");
+            write(connfd, buff, strlen(buff));
+            bzero(buff,1024);
+        }else{
+            printf("Gets into if statement\n");
+            printf("%s", buff);
+            write(connfd , buff , strlen(buff));
+            bzero(buff,1024);
+        }
+
+        while (fgets(buff, sizeof(buff), in) != NULL) {
+            printf("Gets into fgets\n");
+            printf("%s", buff);
+            write(connfd , buff , strlen(buff));
+            bzero(buff,1024);
+        }
+        //int length = (int)strlen(buff);
+        //printf("gets outside while loop %d\n",length);
+        pclose(in);
+
+        printf("\n------------------------\n");
+        //printf(buff);
+
+        //write(connfd , buff , strlen(buff));
+        bzero(buff,1024);
+    }
+     
+    if(read_size == 0)
+    {
+        perror("Client disconnected\n");
+        close(connfd);
+        //close(listenfd);
+        //listenServer(listenfd);
+        //fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed on server\n");
+    }
+}
+
+void acceptReadWriteServer(int listenfd){
+  while(1){
+    int connfd = acceptServer(listenfd);
+    readWriteServer(connfd, listenfd);
+  }
+}
+
+int
+main(int argc, char **argv)
+{
+  numArgs(argc);
+  int listenfd = createListenSocket();
+  createServer(argv[1]);
+  bindServer(listenfd);
+  listenServer(listenfd);
+
+  //while(1){
+  //int connfd = acceptServer(listenfd);
+  //readWriteServer(connfd, listenfd);
+  //}
+
+  acceptReadWriteServer(listenfd);
+  return 0;
+}
